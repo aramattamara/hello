409be1db33940797adff40183bd52b01d3d9abbe
Tamara <ms.vashchuk96@gmail.com>
2022-10-26 00:41:56 -0400

    Refactor: move functions to hello.go

--- hello.go
+++ hello.go
@@ -1,24 +1,24 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+
+	// we have to import the driver, but don't use it in our code
+	// so we use the `_` symbol
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //	func main() {
 //		fmt.Println("Hello, World!")
 //	}
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
-
-func main3() {
-	x, y := split(17)
-	log.Printf("%v, %v", x, y)
+func main() {
+	//callhttp()
+	//dbconnect()
 }
 
 func callhttp() {
@@ -37,3 +37,25 @@ func callhttp() {
 }
 
 func sendsqldata() {}
+
+func dbconnect() {
+	// The `sql.Open` function opens a new `*sql.DB` instance. We specify the driver name
+	// and the URI for our database. Here, we're using a Postgres URI
+	db, err := sql.Open("sqlite3", "db.sqlite")
+	if err != nil {
+		log.Fatalf("could not connect to database: %v", err)
+	}
+
+	// To verify the connection to our database instance, we can call the `Ping`
+	// method. If no error is returned, we can assume a successful connection
+	if err := db.Ping(); err != nil {
+		log.Fatalf("unable to reach database: %v", err)
+	}
+	fmt.Println("database is reachable")
+
+	res, err := db.Exec("INSERT INTO test VALUES(?)", "gbhyv")
+	if err != nil {
+		log.Fatalf("unable to insert data in database: %v", err)
+	}
+	fmt.Printf("insert is succesful %v", res)
+}
